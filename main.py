ce3fbb5a2724a43526485b39526b2d9606a9ea1b
Bhiki Pallai <bhikipallai@Bhikis-MacBook-Air.local>
2024-06-29 10:54:20 +0530

    dvc added

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTraining
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
-
+from cnnClassifier.pipeline.stage_04_model_evaluation_mlflow import EvaluationPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -34,6 +34,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    model_training = ModelTrainingPipeline()
    model_training.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Evaluation"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evaluation = EvaluationPipeline()
+   model_evaluation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,6 +1,7 @@
 from cnnClassifier.constants import *
-from cnnClassifier.utils.common import read_yaml, create_directories
+from cnnClassifier.utils.common import read_yaml, create_directories,save_json
 from cnnClassifier.entity.config_entity import DataIngestionConfig,PrepareBaseModelConfig,TrainingConfig
+from cnnClassifier.entity.config_entity import EvaluationConfig
 
 import os
 
@@ -68,4 +69,13 @@ class ConfigurationManager:
         )
 
         return training_config
-    
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/kidney-ct-scan-image",
+            mlflow_uri="https://dagshub.com/Vicky7873/kidney_Disease_Classification.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
